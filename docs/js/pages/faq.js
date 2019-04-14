58024811aeca2988fcfa723182c1a302e70cadf5
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-04-14 11:22:18 -0300

    Remove obvious duplicates with fn.sass

--- docs/js/pages/faq.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var base_1 = require("../base");
-exports.Faq = base_1.page("\n# How to contribute?\n\nGo to [https://github.com/fabiommendes/mendelev.css/] and make a PR!\n");
--- docs/js/pages/intro.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var base_1 = require("../base");
-exports.Intro = base_1.page("# What is Mendeleev.css?\n\nAn atomic CSS lib.\n");
